91b48a16602ab79d5eed69937b39e025e7c89fcd
karima <elmajdoubkarima01@gmail.com>
2023-12-10 01:37:30 +0100

    first commit

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 
 # Change the value of experience that you want to test
-url = 'http://localhost:5000/api'
+url = 'http://127.0.0.1:5000/api'
 feature = [[5.8, 4.0, 1.2, 0.2]]
 labels ={
   0: "setosa",
