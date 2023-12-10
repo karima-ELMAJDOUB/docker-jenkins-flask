436dac9eb7d38fdd8cda952bcf2775159c8a6025
karima <elmajdoubkarima01@gmail.com>
2023-12-11 00:49:06 +0100

    first commit

--- request.py
+++ request.py
@@ -1,8 +1,11 @@
 import requests
 
-# Change the value of feature that you want to test
+# Change the value of features that you want to test
 url = 'http://127.0.0.1:5000/api'
-feature = {'feature': [[5.8, 4.0, 1.2, 5.2]]}
+features = [[5.8, 4.0, 1.2, 5.2]]
 
-r = requests.post(url, json=feature)
-print(r.json())
+# Send a POST request with the features
+r = requests.post(url, json={'feature': features})
+
+# Print the prediction result
+print(r.text)
