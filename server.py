33ad3d117889bedc487fb57905c5ccdbd60f3311
karima <elmajdoubkarima01@gmail.com>
2023-12-11 19:45:26 +0100

    first commit

--- server.py
+++ server.py
@@ -22,9 +22,9 @@ def predict():
 
 
 # New route for the root path
-@app.route('/')
-def home():
-    return 'Hello, World!'
+#@app.route('/')
+#def home():
+# return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
