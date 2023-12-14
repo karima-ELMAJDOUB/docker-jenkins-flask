729b1857c071e11636ab524eecf945b02b71c094
karima <elmajdoubkarima01@gmail.com>
2023-12-14 21:59:25 +0100

    first commit

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ pickle.dump(classifier, open('model.pkl','wb'))
 
 #load the model and test with a custom input
 model = pickle.load( open('model.pkl','rb'))
-x = [[6.7, 3.3, 5.7, 2.1]]
+x = [[9.8, 7.8, 5.7, 3.4]]
 predict = model.predict(x)
 print("Hello Worlds")
 print(labels[predict[0]])
